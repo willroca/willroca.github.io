365730b41b768fb93ec1ba87e29daa56cae22911
willroca <36712822+willroca@users.noreply.github.com>
2020-10-26 12:57:00 -0300

    Initial commit
    Reference: https://gist.github.com/TylerFisher/6127328

---
+++ static/darkmode.js
@@ -0,0 +1,12 @@
+function darkmode() {
+    if (document.getElementById('darkSwitch').checked == true){
+        var element = document.body;
+        element.classList.toggle("dark-mode");
+        document.querySelector('.jumbotron').style.backgroundColor = '#1e2125';
+    } 
+    else {
+        var elementoso = document.body;
+        elementoso.classList.toggle("dark-mode",false);
+        document.querySelector('.jumbotron').style.backgroundColor = '#E9ECEF';
+    }
+}

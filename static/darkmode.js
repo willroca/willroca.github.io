4cb30725407aff7d3555fd7647e350e9a49a2909
willroca <36712822+willroca@users.noreply.github.com>
2020-10-28 18:25:19 -0300

    update continuous dark mode

--- static/darkmode.js
+++ static/darkmode.js
@@ -3,10 +3,12 @@ function darkmode() {
         var element = document.body;
         element.classList.toggle("dark-mode");
         document.querySelector('.jumbotron').style.backgroundColor = '#1e2125';
-    } 
+        window.localStorage.setItem('darkmode', 'true');
+    }
     else {
         var elementoso = document.body;
         elementoso.classList.toggle("dark-mode",false);
         document.querySelector('.jumbotron').style.backgroundColor = '#E9ECEF';
+        window.localStorage.setItem('darkmode', 'false');
     }
 }
